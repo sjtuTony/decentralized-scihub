78fa5f1e44a058677f693595c17965b349b4ae75
sjtuTony <tonycode2012@163.com>
2021-10-21 14:53:27 +0800

    Add check ci

---
+++ .github/etc/check.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+function isValidCid()
+{
+    local tmpCid=$1
+    if ! [[ $tmpCid =~ ^Qm ]]; then
+        return 1
+    fi
+    if [ ${#tmpCid} -ne 46 ]; then
+        return 1
+    fi
+
+    return 0
+}
+
+########## MAIN BODY ##########
+basedir=$(cd `dirname $0`;pwd)
+files=($1)
+maxNum=101
+maxSize=$((5 * 1024 * 1024 * 1024))
+cidTag='(?<=papers/).*(?=/)'
+JQ=$basedir/jq
+
+if [ ! -f "$JQ" ]; then 
+    exit 1
+fi
+
+### Check if there is only one meta file
+cidArry=($(printf '%s\n' "${files[@]}" | grep -Po $cidTag | sort | uniq))
+if [ ${#cidArry[*]} -ne 1 ]; then
+    echo "Can just upload one Qmxxx folder, but there are two"
+    exit 1
+fi
+cidRoot=${cidArry[0]}
+
+### Check if paper number is valid
+papers=$(printf '%s\n' "${files[@]}" | grep "papers/$cidRoot/" | grep -v "meta")
+papersNum=${#papers[*]}
+if [ $papersNum -gt $maxNum ] || [ $papersNum -le 0 ]; then
+    echo "Upload file number should range (0, $maxNum]"
+    exit 1
+fi
+
+### Check meta
+metaFile="papers/$cidRoot/meta" 
+if [ ! -f "$metaFile" ]; then
+    echo "meta file not found"
+    exit 1
+fi
+cidRootGet=$(cat $metaFile | $JQ -r .cid)
+isValidCid $cidRootGet || { exit 1; }
+totalSizeGet=$(cat $metaFile | $JQ -r .size)
+countSize=0
+declare -A doi2cid
+subCids=($(cat $metaFile | jq -r '.links|.[]|.cid'))
+index=0
+## Check if cid is right
+for doi in $(cat $metaFile | jq -r '.links|.[]|.doi'); do
+    doiFile="papers/$cidRoot/$doi"
+    if [ ! -f "$doiFile" ]; then
+        echo "doi file:$doiFile not found"
+        exit 1
+    fi
+    cidGet=$(cat $doiFile | $JQ -r .cid)
+    isValidCid $cidGet || { exit 1; }
+    sizeGet=$(cat $doiFile | $JQ -r .size)
+    if [ x"$cidGet" != x"${subCids[$index]}" ]; then
+        echo "doi file:$doiFile cid:$cidGet not equal to meta cid:${subCids[$index]}"
+        exit 1
+    fi
+    ((index++))
+    ((countSize += sizeGet))
+done
+## Check if size is right
+if [ $totalSizeGet -le $countSize ]; then
+    echo "Wrong final size"
+    exit 1
+fi
+if [ $totalSizeGet -gt $maxSize ]; then
+    echo "Total size exceeds $maxSize"
+    exit 1
+fi
